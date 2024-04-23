b3783d6bbb3a32e9c193f6ffc0b2663b5a7a7d00
Luan Pereira <luanpsmorris@gmail.com>
2024-04-23 19:36:32 -0300

    feat: envia mensagens para uma lista de contatos

---
+++ sendMsn.js
@@ -0,0 +1,62 @@
+#!/usr/bin/node
+
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+
+    function enviarScript(scriptText){
+        const lines = scriptText.split("\n");
+
+        let i = 0;
+
+        setInterval(() => {
+            if(i >= lines.length) return
+
+            if(lines[i].trim() != ''){
+                console.log(lines[i]);
+
+                const number = lines[i];
+
+                const title = '*NOME DO CURSO*'; //Coloque entre ** para negrito
+                const institution = 'da Instituição'; //Coloque junto com a preposição
+                const address = 'Endereço: Tv. Vitorino Freire, s/n , Areal, Coroatá - MA';
+                const whatsappGroup = "https://chat.whatsapp.com/0000000000000000000000";
+
+                const message = `${title}\nBom dia. Aqui é do corpo técnico ${institution}. Estamos enviando o link para o grupo de whatsapp dos alunos que servirá para o envio de materiais, avisos, dúvidas e etc.\n\nSeja bem vindo e bons estudos.\n\n${address}\n\n${whatsappGroup}`;
+
+                const notice = "Caso já faça parte do grupo desconsidere essa mensagem";
+
+                const chatId = number + "@c.us";
+
+                client.sendMessage(chatId, message);
+                client.sendMessage(chatId, notice);
+
+            }
+            i++;
+        }, 250)
+    }
+
+    enviarScript(`
+559984222755
+`)
+});
+
+client.initialize();
+
+client.on('message', message => {
+
+    console.log('');
+    console.log(message.from);
+    console.log(message.body);
+    console.log(message.id.participant);
+
+});
